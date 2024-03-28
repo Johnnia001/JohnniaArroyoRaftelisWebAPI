1334cc5e8cacca9d514ccf8f65791220a73988e8
johnnia001 <jarroyo.staff@gmail.com>
2024-03-27 22:31:48 -0700

    Add Parcels.txt and Model. Setup Structure.

---
+++ JohnniaArroyoRaftelisWebAPI/BackendAPI/Models/Parcel.cs
@@ -0,0 +1,12 @@
+
+public class Parcel
+{
+    public string Pin { get; set; }
+    public string Address { get; set; }
+    public string Owner { get; set; }
+    public decimal MarketValue { get; set; }
+    public DateTime SaleDate { get; set; }
+    public decimal SalePrice { get; set; }
+    public string Link { get; set; }
+    public string GoogleMapsLink { get; set; }
+}
